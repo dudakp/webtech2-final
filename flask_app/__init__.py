26c278b1451d6b63d7f9355dd919f199a108a049
peterhajduk <nexx458@gmail.com>
2020-04-28 00:10:38 +0200

    cors added

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 
 from flask import Flask
+from flask_cors import CORS
 from flask_login import LoginManager
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
@@ -10,12 +11,14 @@ config_json_path = os.path.join(str(pathlib.Path.cwd()), 'flask_app/config.json'
 mongo = PyMongo()
 b_crypt = Bcrypt()
 login_manager = LoginManager()
+cors = CORS()
 # login_manager.login_view('login')
 
 
 def create_app(config_json=config_json_path):
     app = Flask(__name__)
     app.config.from_json(config_json, silent=False)
+    CORS().init_app(app)
     mongo.init_app(app)
     b_crypt.init_app(app)
     login_manager.init_app(app)
