196110ca6eeda4f06894272a2747ab12b6303286
Malph1 <v.peter18@gmail.com>
2020-06-08 18:05:11 +0200

    preklady animacia

--- flask_app/static/ball-animation.js
+++ flask_app/static/ball-animation.js
@@ -1,1 +1,47 @@
-console.log('Oh god please someone animate me');
+window.canvasInfo = {
+    width: 640,
+    height: 640
+};
+
+$(document).ready(() => {
+    window.app = new PIXI.Application({
+        backgroundColor: 0x1099bb,
+        width: canvasInfo.width, height: canvasInfo.height
+    });
+    document.getElementById('canvas').appendChild(window.app.view);
+
+
+    const ballSprite = PIXI.Sprite.from(window.ball.src);
+    ballSprite.anchor.set(0.5);
+    ballSprite.x = window.app.screen.width / 2;
+    ballSprite.y = window.app.screen.height / 2 + 70;
+
+    const barSprite = PIXI.Sprite.from(window.bar.src);
+    barSprite.anchor.set(0.5);
+    barSprite.x = window.app.screen.width / 2;
+    barSprite.y = (window.app.screen.height /2)+100;
+
+
+
+
+    window.createBallSprite = () => {
+        window.app.stage.addChild(ballSprite);
+    };
+
+
+    window.createBarSprite = () => {
+        window.app.stage.addChild(barSprite);
+    };
+
+
+    createBallSprite();
+    createBarSprite();
+
+    window.app.ticker.add((delta) => {
+       ballSprite.x = 250+ window.ball.currentPosition*100;
+       // ballSprite.y = window.ball.currentPosition*1000;
+
+       barSprite.rotation = window.bar.currentTilt*100;
+
+    });
+});
