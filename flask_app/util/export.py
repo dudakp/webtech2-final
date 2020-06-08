6143fd071f7c04ab1af9e67a52a7c51406500d17
dudakp <palodudak@gmail.com>
2020-06-08 13:18:04 +0200

    hotfix - opravene exportovanie statistik

--- flask_app/util/export.py
+++ flask_app/util/export.py
@@ -15,25 +15,31 @@ class DBExporter(metaclass=Singleton):
         del df['_id']
         df.to_csv(str(Path(__file__).parent.absolute()) + '/../static/export.csv', index=False, header=True)
 
-    def all_to_html(self):
-        df = pd.DataFrame(list(self.db.find({})))
-        del df['_id']
+    def all_to_html(self, dataframe=None):
         root = str(Path(__file__).parent.absolute())
-        df.to_html(root + '/../static/export.html', index=False, header=True)
+        if dataframe is None:
+            df = pd.DataFrame(list(self.db.find({})))
+            del df['_id']
+            df.to_html(root + '/../static/export.html', index=False, header=True)
+            print('exporting invoked in route')
+        print('exporting invoked by client')
+        dataframe.to_html(root + '/../static/stats.html', index=False, header=True)
 
-    def all_to_pdf(self):
+    def all_to_pdf(self, dataframe=None):
         root = str(Path(__file__).parent.absolute())
-        self.all_to_html()
-        pdfkit.from_file(root + '/../static/export.html', root + '/../static/export.pdf')
+        if dataframe is None:
+            self.all_to_html()
+            pdfkit.from_file(root + '/../static/export.html', root + '/../static/export.pdf')
+        self.all_to_html(dataframe)
+        pdfkit.from_file(root + '/../static/stats.html', root + '/../static/stats.pdf')
 
     def compute_stats(self):
         # pocet volani celkovo, pocet uspesnych, pocet neuspesnych
         df = pd.DataFrame(list(self.db.find({})))
-        hist = df.groupby('command').command.count().to_dict()
-        results = df.groupby('result').result.count().to_dict()
+        hist = df.groupby('command').command.count()
+        results = df.groupby('result').result.count()
         total_calls = df['_id'].count()
-        import pprint
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(hist)
-        pp.pprint(total_calls)
-        return hist, results, total_calls
+        df_complete_stats = pd.concat(
+            [hist, results, pd.DataFrame.from_dict({'TOTAL_CALLS': total_calls}, orient='index')]).reset_index()
+        self.all_to_pdf(df_complete_stats)
+        return hist.to_dict(), results.to_dict(), total_calls
