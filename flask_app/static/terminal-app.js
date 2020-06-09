f48571fdb923aa69b89f58598921d9e8dd207b50
Ajordon11 <andrej.fula@gmail.com>
2020-06-09 21:55:20 +0200

    Add page to link as it should have been from the start (also removed my sexy function, that I spent half day on)

--- flask_app/static/terminal-app.js
+++ flask_app/static/terminal-app.js
@@ -3,25 +3,25 @@ let userPrefix;
 const commandInput = $('#command-input');
 const prefixElem = $('.command-prefix');
 
-const setActiveNavLink = () => {
-    // remove active class and current tag
-    $('.nav-link').removeClass('active');
-    $('.nav-link:first').html('Simulacie');
-    $('.nav-link:last').html('Octave terminal');
-
-    let activeLink;
-    const srCurrent = '<span class="sr-only">(current)</span>';
-    if (window.location.pathname === '/terminal') {
-        activeLink = $('.nav-link:last');
-    } else {
-        activeLink = $('.nav-link:first');
-    }
-    activeLink.append(srCurrent);
-    activeLink.addClass('active');
-};
+// const setActiveNavLink = () => {
+//     // remove active class and current tag
+//     $('.nav-link').removeClass('active');
+//     $('.nav-link:first').html('Simulacie');
+//     $('.nav-link:last').html('Octave terminal');
+//
+//     let activeLink;
+//     const srCurrent = '<span class="sr-only">(current)</span>';
+//     if (window.location.pathname === '/terminal') {
+//         activeLink = $('.nav-link:last');
+//     } else {
+//         activeLink = $('.nav-link:first');
+//     }
+//     activeLink.append(srCurrent);
+//     activeLink.addClass('active');
+// };
 
 $(document).ready(() => {
-    setActiveNavLink();
+    // setActiveNavLink();
     createUserPrefix();
     $('#mail-error-message').hide();
     $('#mail-success-message').hide();
